1f6181582919c645817a228375a30e83679acd25
Dave Cross <d.cross@fairfx.com>
2019-08-16 15:55:49 +0100

    Added (basic) Pod

--- lib/AudioFile/Info/Dummy.pm
+++ lib/AudioFile/Info/Dummy.pm
@@ -1,8 +1,28 @@
+=head1 NAME
+
+AudioFile::Info::Dummy
+
+=head1 DESCRIPTION
+
+Dummy plugin for testing AudioFile::Info.
+
+=head1 SYNOPSIS
+
+Don't use this.
+
+=cut
+
 package AudioFile::Info::Dummy;
 
 use 5.006;
 use strict;
 use warnings;
+
+=head1 METHODS
+
+=head2 new, artist, title, album, track, year, genre
+
+=cut
 
 sub new {
   return bless {}, $_[0];
